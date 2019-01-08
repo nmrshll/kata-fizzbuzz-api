aa8ff811bb163932b19b388fe46aa6634c44cde6
nmrshll <marshall.nicolas@gmail.com>
2019-01-08 14:17:33 +0100

    fizzbuzz service

---
+++ services/fizzbuzz_service.go
@@ -0,0 +1,25 @@
+package services
+
+import "fmt"
+
+type FizzBuzzService struct {
+}
+
+func (fbservice *FizzBuzzService) FizzBuzz(limit, int1, int2 int, str1, str2 string) string {
+	results := ""
+	for i := 1; i <= limit; i++ {
+		result := ""
+		if i%int1 == 0 {
+			result += str1
+		}
+		if i%int2 == 0 {
+			result += str2
+		}
+		if result != "" {
+			results += result + "\n"
+			continue
+		}
+		results += fmt.Sprintf("%d\n", i)
+	}
+	return results
+}

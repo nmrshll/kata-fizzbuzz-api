b5cb798aea50afbbbc4d35adf04e7e4cd78919cf
nmrshll <marshall.nicolas@gmail.com>
2019-01-08 18:03:18 +0100

    POST /fizzbuzz reponds 200

--- api/fizzBuzzRoutes.go
+++ api/fizzBuzzRoutes.go
@@ -7,10 +7,13 @@ import (
 	"github.com/nmrshll/kata-fizzbuzz-api/services/fizzbuzzservice"
 )
 
+// FizzBuzzRoutes holds all fizzbuzz-routes related methods in one dependency
 type FizzBuzzRoutes struct {
 	fizzBuzzService fizzbuzzservice.FizzBuzzService
 }
 
+// POSTFizzBuzz responds to POST /fizzbuzz
+// expects a body with all 5 parameters (limit,int1,int2,str1,str2) (see fixzzbuzzservice.POSTFizzBuzzParameters)
 func (r *FizzBuzzRoutes) POSTFizzBuzz(c echo.Context) error {
 	params := &fizzbuzzservice.POSTFizzBuzzParameters{}
 	if err := c.Bind(params); err != nil {
@@ -19,5 +22,5 @@ func (r *FizzBuzzRoutes) POSTFizzBuzz(c echo.Context) error {
 
 	fizzBuzzOutput := r.fizzBuzzService.FizzBuzz(15, 3, 5, "fizz", "buzz")
 
-	return c.JSON(http.StatusCreated, fizzBuzzOutput)
+	return c.JSON(http.StatusOK, fizzBuzzOutput)
 }
